b89eed44ea14f60e4c9f210e00ac4497291ca809
osy <osy@turing.llc>
2025-11-28 18:51:10 -0800

    vkr: support importing MTLHeap memory

--- src/venus/vkr_context.h
+++ src/venus/vkr_context.h
@@ -29,6 +29,8 @@ struct vkr_resource {
       int fd;
       /* valid when fd_type is shm */
       uint8_t *data;
+      /* valid when fd_type is metal heap */
+      void *metal_heap;
    } u;
 
    size_t size;
