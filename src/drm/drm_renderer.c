Vera-Firefly/Virglrenderer-test

commit 7f9e2d8a61d84a15cb428e7f56db2dce739f9e5d

drm: Add drm native context support
Add a new context type to allow native mesa drivers in guest to access
host drm driver.  This just adds the basic skeleton, plus a generic
fence/timeline implementation suitable for dealing with dma-fence fd's.
The next patch will add the first drm native context implementation.

Signed-off-by: Rob Clark <robdclark@chromium.org>
Reviewed-by: Dmitry Osipenko <dmitry.osipenko@collabora.com>
Reviewed-by: Ryan Neph <ryanneph@google.com>
Reviewed-by: Yiwei Zhang <zzyiwei@chromium.org>
Reviewed-by: Chia-I Wu <olvaffe@gmail.com>
Part-of: <https://gitlab.freedesktop.org/virgl/virglrenderer/-/merge_requests/693>

src/drm/drm_renderer.c
@@ -1,0 +1,125 @@
+/*
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: MIT
+ */
+
+#include "config.h"
+
+#include <errno.h>
+#include <inttypes.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <unistd.h>
+
+#include <xf86drm.h>
+
+#include "drm_hw.h"
+#include "drm_renderer.h"
+
+static struct virgl_renderer_capset_drm capset;
+
+struct backend {
+   uint32_t context_type;
+   const char *name;
+   int (*probe)(int fd, struct virgl_renderer_capset_drm *capset);
+   struct virgl_context *(*create)(int fd);
+} backends[] = {
+};
+
+int
+drm_renderer_init(int drm_fd)
+{
+   for (unsigned i = 0; i < ARRAY_SIZE(backends); i++) {
+      const struct backend *b = &backends[i];
+      int fd;
+
+      if (drm_fd != -1) {
+         fd = drm_fd;
+      } else {
+         fd = drmOpenWithType(b->name, NULL, DRM_NODE_RENDER);
+         if (fd < 0)
+            continue;
+      }
+
+      drmVersionPtr ver = drmGetVersion(fd);
+      if (!ver) {
+         close(fd);
+         return -ENOMEM;
+      }
+
+      if (strcmp(ver->name, b->name)) {
+         /* In the drmOpenWithType() path, we will only get back an fd
+          * for the device with matching name.  But when we are using
+          * an externally provided fd, we need to go thru the backends
+          * table to see which one has the matching name.
+          */
+         assert(drm_fd != -1);
+         drmFreeVersion(ver);
+         continue;
+      }
+
+      capset.version_major = ver->version_major;
+      capset.version_minor = ver->version_minor;
+      capset.version_patchlevel = ver->version_patchlevel;
+      capset.context_type = b->context_type;
+
+      int ret = b->probe(fd, &capset);
+      if (ret)
+         memset(&capset, 0, sizeof(capset));
+
+      drmFreeVersion(ver);
+      close(fd);
+      return ret;
+   }
+
+   if (drm_fd != -1)
+      close(drm_fd);
+
+   return -ENODEV;
+}
+
+void
+drm_renderer_fini(void)
+{
+   drm_log("");
+}
+
+void
+drm_renderer_reset(void)
+{
+   drm_log("");
+}
+
+size_t
+drm_renderer_capset(void *_c)
+{
+   struct virgl_renderer_capset_drm *c = _c;
+   drm_log("c=%p", c);
+
+   if (!capset.context_type)
+      return 0;
+
+   if (c)
+      *c = capset;
+
+   return sizeof(*c);
+}
+
+struct virgl_context *
+drm_renderer_create(UNUSED size_t debug_len, UNUSED const char *debug_name)
+{
+   for (unsigned i = 0; i < ARRAY_SIZE(backends); i++) {
+      const struct backend *b = &backends[i];
+
+      if (b->context_type != capset.context_type)
+         continue;
+
+      int fd = drmOpenWithType(b->name, NULL, DRM_NODE_RENDER);
+      if (fd < 0)
+         return NULL;
+
+      return b->create(fd);
+   }
+
+   return NULL;
+}

config.h.meson
@@ -34,6 +34,7 @@
 #mesondefine ENABLE_MINIGBM_ALLOCATION
 #mesondefine ENABLE_VENUS
 #mesondefine ENABLE_VENUS_VALIDATE
+#mesondefine ENABLE_DRM
 #mesondefine ENABLE_RENDER_SERVER
 #mesondefine ENABLE_RENDER_SERVER_WORKER_PROCESS
 #mesondefine ENABLE_RENDER_SERVER_WORKER_THREAD

meson.build
@@ -250,6 +250,8 @@ if with_venus
    endif
 endif
 
+with_drm = false
+
 with_render_server = get_option('render-server')
 with_render_server_worker = get_option('render-server-worker')
 render_server_install_dir = get_option('prefix') / get_option('libexecdir')

src/drm/.clang-format
@@ -1,0 +1,137 @@
+AlignAfterOpenBracket: true
+AlignConsecutiveMacros: true
+AlignConsecutiveBitFields: true
+AllowAllArgumentsOnNextLine: false
+AllowShortCaseLabelsOnASingleLine: false
+AllowShortFunctionsOnASingleLine: false
+AlwaysBreakAfterReturnType: TopLevelDefinitions
+BasedOnStyle: LLVM
+BraceWrapping:
+  AfterControlStatement: false
+  AfterEnum: false
+  AfterFunction: true
+  AfterStruct: false
+  BeforeElse: false
+  SplitEmptyFunction: true
+BinPackArguments: true
+BinPackParameters: true
+BreakBeforeBraces: Custom
+ColumnLimit: 90
+ContinuationIndentWidth: 3
+Cpp11BracedListStyle: false
+Cpp11BracedListStyle: true
+BreakStringLiterals: false
+ForEachMacros:
+  - BITSET_FOREACH_SET
+  - BITSET_FOREACH_RANGE
+  - LIST_FOR_EACH_ENTRY
+  - LIST_FOR_EACH_ENTRY_SAFE
+  - LIST_FOR_EACH_ENTRY_SAFE_REV
+  - list_for_each_entry
+  - list_for_each_entry_safe
+  - list_for_each_entry_rev
+  - list_for_each_entry_rev_safe
+  - list_for_each_entry_from
+  - list_for_each_entry_from_rev
+  - foreach_list_typed
+  - u_foreach_bit
+  - util_dynarray_foreach
+  - rb_tree_foreach
+  - rb_tree_foreach_safe
+  - nir_foreach_variable
+  - nir_foreach_variable_safe
+  - nir_foreach_uniform_variable
+  - nir_foreach_uniform_variable_safe
+  - nir_foreach_register
+  - nir_foreach_register_safe
+  - nir_foreach_use
+  - nir_foreach_use_safe
+  - nir_foreach_if_use
+  - nir_foreach_if_use_safe
+  - nir_foreach_def
+  - nir_foreach_def_safe
+  - nir_foreach_phi_src
+  - nir_foreach_phi_src_safe
+  - nir_foreach_parallel_copy_entry
+  - nir_foreach_instr
+  - nir_foreach_instr_reverse
+  - nir_foreach_instr_safe
+  - nir_foreach_instr_reverse_safe
+  - nir_foreach_instr_from_safe
+  - nir_foreach_function
+  - nir_foreach_block
+  - nir_foreach_block_safe
+  - nir_foreach_block_reverse
+  - nir_foreach_block_reverse_safe
+  - nir_foreach_block_in_cf_node
+  - nir_foreach_shader_in_variable
+  - nir_foreach_shader_out_variable_safe
+  - nir_foreach_variable_in_list
+  - nir_foreach_variable_with_modes_safe
+  - nir_foreach_variable_with_modes
+  - nir_foreach_shader_out_variable
+  - ir2_foreach_instr
+  - ir2_foreach_live_reg
+  - ir2_foreach_avail
+  - ir2_foreach_src
+  - foreach_two_lists
+  - foreach_bit
+  - foreach_sched_node
+  - foreach_src
+  - foreach_src_n
+  - foreach_dst
+  - foreach_dst_n
+  - ra_foreach_dst
+  - ra_foreach_src
+  - ra_foreach_src_rev
+  - foreach_ssa_use
+  - foreach_ssa_srcp_n
+  - foreach_ssa_srcp
+  - foreach_ssa_src_n
+  - foreach_ssa_src
+  - foreach_input_n
+  - foreach_input
+  - foreach_output_n
+  - foreach_output
+  - foreach_instr
+  - foreach_instr_rev
+  - foreach_instr_safe
+  - foreach_instr_from_safe
+  - foreach_block
+  - foreach_block_safe
+  - foreach_block_rev
+  - foreach_array
+  - foreach_array_safe
+  - foreach_name_n
+  - foreach_name
+  - foreach_def
+  - foreach_use
+  - foreach_interval
+  - foreach_interval_safe
+  - foreach_interval_rev
+  - foreach_interval_rev_safe
+  - foreach_batch
+  - hash_table_foreach
+  - set_foreach
+  - foreach_line_in_section
+  - perf_time
+  - perf_time_ctx
+  - foreach_submit
+  - foreach_submit_safe
+
+IncludeBlocks: Preserve
+IncludeCategories:
+  - Regex:           '<[[:alnum:].]+>'
+    Priority:        1
+  - Regex:           '.*\/.*'
+    Priority:        2
+  - Regex:           '.*'
+    Priority:        3
+IndentWidth: 3
+PenaltyBreakBeforeFirstCallParameter: 1
+PenaltyExcessCharacter: 100
+SpaceAfterCStyleCast: false
+SpaceBeforeCpp11BracedList: false
+SpaceBeforeCtorInitializerColon: false
+SpacesInContainerLiterals: false
+

src/drm/drm_fence.c
@@ -1,0 +1,169 @@
+/*
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <poll.h>
+#include <string.h>
+
+#include "virgl_context.h"
+#include "virgl_util.h"
+
+#include "util/os_file.h"
+#include "util/u_atomic.h"
+#include "util/u_thread.h"
+
+#include "drm_fence.h"
+#include "drm_util.h"
+
+/**
+ * Tracking for a single fence on a timeline
+ */
+struct drm_fence {
+   int fd;
+   uint32_t flags;
+   void *fence_cookie;
+   struct list_head node;
+};
+
+static void
+drm_fence_destroy(struct drm_fence *fence)
+{
+   close(fence->fd);
+   list_del(&fence->node);
+   free(fence);
+}
+
+static struct drm_fence *
+drm_fence_create(int fd, uint32_t flags, void *fence_cookie)
+{
+   struct drm_fence *fence = calloc(1, sizeof(*fence));
+
+   if (!fence)
+      return NULL;
+
+   fence->fd = os_dupfd_cloexec(fd);
+
+   if (fence->fd < 0) {
+      free(fence);
+      return NULL;
+   }
+
+   fence->flags = flags;
+   fence->fence_cookie = fence_cookie;
+
+   return fence;
+}
+
+static int
+thread_sync(void *arg)
+{
+   struct drm_timeline *timeline = arg;
+
+   u_thread_setname(timeline->name);
+
+   mtx_lock(&timeline->fence_mutex);
+   while (!timeline->stop_sync_thread) {
+      if (list_is_empty(&timeline->pending_fences)) {
+         if (cnd_wait(&timeline->fence_cond, &timeline->fence_mutex))
+            drm_log("error waiting on fence condition");
+         continue;
+      }
+
+      struct drm_fence *fence =
+         list_first_entry(&timeline->pending_fences, struct drm_fence, node);
+      int ret;
+
+      mtx_unlock(&timeline->fence_mutex);
+      ret = poll(&(struct pollfd){fence->fd, POLLIN}, 1, -1); /* wait forever */
+      mtx_lock(&timeline->fence_mutex);
+
+      if (ret == 1) {
+         drm_dbg("fence signaled: %p (%p)", fence, fence->fence_cookie);
+         timeline->vctx->fence_retire(timeline->vctx, timeline->ring_idx,
+                                      fence->fence_cookie);
+         write_eventfd(timeline->eventfd, 1);
+         drm_fence_destroy(fence);
+      } else if (ret != 0) {
+         drm_log("poll failed: %s", strerror(errno));
+      }
+   }
+   mtx_unlock(&timeline->fence_mutex);
+
+   return 0;
+}
+
+void
+drm_timeline_init(struct drm_timeline *timeline, struct virgl_context *vctx,
+                  const char *name, int eventfd, int ring_idx)
+{
+   timeline->vctx = vctx;
+   timeline->name = name;
+   timeline->eventfd = eventfd;
+   timeline->ring_idx = ring_idx;
+
+   timeline->last_fence_fd = -1;
+
+   list_inithead(&timeline->pending_fences);
+
+   mtx_init(&timeline->fence_mutex, mtx_plain);
+   cnd_init(&timeline->fence_cond);
+
+   timeline->sync_thread = u_thread_create(thread_sync, timeline);
+}
+
+void
+drm_timeline_fini(struct drm_timeline *timeline)
+{
+   /* signal thread_sync to shutdown: */
+   mtx_lock(&timeline->fence_mutex);
+   timeline->stop_sync_thread = true;
+   cnd_signal(&timeline->fence_cond);
+   mtx_unlock(&timeline->fence_mutex);
+
+   /* wait for thread_sync to exit: */
+   thrd_join(timeline->sync_thread, NULL);
+
+   if (timeline->last_fence_fd != -1)
+      close(timeline->last_fence_fd);
+
+   /* cleanup remaining fences: */
+   list_for_each_entry_safe (struct drm_fence, fence, &timeline->pending_fences, node) {
+      drm_fence_destroy(fence);
+   }
+
+   cnd_destroy(&timeline->fence_cond);
+   mtx_destroy(&timeline->fence_mutex);
+}
+
+int
+drm_timeline_submit_fence(struct drm_timeline *timeline, uint32_t flags,
+                          void *fence_cookie)
+{
+   if (timeline->last_fence_fd == -1)
+      return -EINVAL;
+
+   struct drm_fence *fence =
+      drm_fence_create(timeline->last_fence_fd, flags, fence_cookie);
+
+   if (!fence)
+      return -ENOMEM;
+
+   drm_dbg("fence: %p (%p)", fence, fence->fence_cookie);
+
+   mtx_lock(&timeline->fence_mutex);
+   list_addtail(&fence->node, &timeline->pending_fences);
+   cnd_signal(&timeline->fence_cond);
+   mtx_unlock(&timeline->fence_mutex);
+
+   return 0;
+}
+
+/* takes ownership of the fd */
+void
+drm_timeline_set_last_fence_fd(struct drm_timeline *timeline, int fd)
+{
+   if (timeline->last_fence_fd != -1)
+      close(timeline->last_fence_fd);
+   timeline->last_fence_fd = fd;
+}

src/drm/drm_fence.h
@@ -1,0 +1,52 @@
+/*
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef DRM_FENCE_H_
+#define DRM_FENCE_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+
+#include "c11/threads.h"
+#include "util/list.h"
+
+/*
+ * Helpers to deal with managing dma-fence fd's.  This should something that
+ * can be re-used by any virtgpu native context implementation.
+ */
+
+struct drm_fence;
+struct virgl_context;
+
+/**
+ * Represents a single timeline of fence-fd's.  Fences on a timeline are
+ * signaled in FIFO order.
+ */
+struct drm_timeline {
+   struct virgl_context *vctx;
+   const char *name;
+   int eventfd;
+   int ring_idx;
+
+   int last_fence_fd;
+   struct list_head pending_fences;
+
+   mtx_t fence_mutex;
+   cnd_t fence_cond;
+   thrd_t sync_thread;
+   bool stop_sync_thread;
+};
+
+void drm_timeline_init(struct drm_timeline *timeline, struct virgl_context *vctx,
+                       const char *name, int eventfd, int ring_idx);
+
+void drm_timeline_fini(struct drm_timeline *timeline);
+
+int drm_timeline_submit_fence(struct drm_timeline *timeline, uint32_t flags,
+                              void *fence_cookie);
+
+void drm_timeline_set_last_fence_fd(struct drm_timeline *timeline, int fd);
+
+#endif /* DRM_FENCE_H_ */

src/drm/drm_renderer.h
@@ -1,0 +1,60 @@
+/*
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: MIT
+ */
+
+#ifndef DRM_RENDERER_H_
+#define DRM_RENDERER_H_
+
+#include "config.h"
+
+#include <inttypes.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#ifdef ENABLE_DRM
+
+int drm_renderer_init(int drm_fd);
+
+void drm_renderer_fini(void);
+
+void drm_renderer_reset(void);
+
+size_t drm_renderer_capset(void *capset);
+
+struct virgl_context *drm_renderer_create(size_t debug_len, const char *debug_name);
+
+#else /* ENABLE_DRM_MSM */
+
+static inline int
+drm_renderer_init(UNUSED int drm_fd)
+{
+   virgl_log("DRM native context support was not enabled in virglrenderer\n");
+   return -1;
+}
+
+static inline void
+drm_renderer_fini(void)
+{
+}
+
+static inline void
+drm_renderer_reset(void)
+{
+}
+
+static inline size_t
+drm_renderer_capset(UNUSED void *capset)
+{
+   return 0;
+}
+
+static inline struct virgl_context *
+drm_renderer_create(UNUSED size_t debug_len, UNUSED const char *debug_name)
+{
+   return NULL;
+}
+
+#endif /* ENABLE_DRM */
+
+#endif /* DRM_RENDERER_H_ */

src/drm/drm_util.c
@@ -1,0 +1,48 @@
+/*
+ * Copyright 2022 Google LLC
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <stdarg.h>
+#include <string.h>
+
+#include "drm_util.h"
+#include "virgl_util.h"
+
+#include "util/macros.h"
+
+void
+_drm_log(const char *fmt, ...)
+{
+   const char prefix[] = "drm: ";
+   char line[1024];
+   size_t len;
+   va_list va;
+   int ret;
+
+   len = ARRAY_SIZE(prefix) - 1;
+   memcpy(line, prefix, len);
+
+   va_start(va, fmt);
+   ret = vsnprintf(line + len, ARRAY_SIZE(line) - len, fmt, va);
+   va_end(va);
+
+   if (ret < 0) {
+      const char log_error[] = "log error";
+      memcpy(line + len, log_error, ARRAY_SIZE(log_error) - 1);
+      len += ARRAY_SIZE(log_error) - 1;
+   } else if ((size_t)ret < ARRAY_SIZE(line) - len) {
+      len += ret;
+   } else {
+      len = ARRAY_SIZE(line) - 1;
+   }
+
+   /* make room for newline */
+   if (len + 1 >= ARRAY_SIZE(line))
+      len--;
+
+   line[len++] = '\n';
+   line[len] = '\0';
+
+   virgl_log(line);
+}

axiphi/virglrenderer

commit 95d040bd5972bb3d72a8aabce85bee537087473d

drm: avoid slow render-node lookup during init
drm_renderer_init() uses drmOpenWithType() to find a render node for
each enabled native-context backend. The legacy lookup scans every
possible minor and waits for udev on missing paths. In a combined AMDGPU
and i915 build on an Intel host, probing the absent AMDGPU backend can
therefore delay the capset response long enough for the guest to time
out.

Probe the render-node path range directly with open() and match each
accessible node's driver name. This keeps missing-node failures
immediate, preserves support for execute-only DRM directories, and tries
every compiled backend before waiting so an absent backend does not
delay one that is already available.

Retain protection against a render node appearing concurrently with
initialization by rescanning at roughly one-millisecond intervals,
bounded by a 50-millisecond monotonic deadline. This covers the startup
race without restoring per-minor waits or falling back to legacy
name-based lookup.

Fixes: 7f9e2d8a61d8 ("drm: Add drm native context support")
Signed-off-by: Joseph Kogut <joseph@anodize.com>

src/drm/drm_renderer.c
@@ -6,10 +6,13 @@
 #include "config.h"
 
 #include <errno.h>
+#include <fcntl.h>
 #include <inttypes.h>
 #include <stddef.h>
+#include <stdio.h>
 #include <stdint.h>
 #include <string.h>
+#include <time.h>
 #include <unistd.h>
 
 #include <xf86drm.h>
@@ -39,6 +42,10 @@
 #endif
 
 static struct virgl_renderer_capset_drm capset;
+
+#define DRM_RENDER_NODE_RESCAN_TIMEOUT_NS UINT64_C(50000000)
+#define DRM_RENDER_NODE_RESCAN_INTERVAL_NS UINT64_C(1000000)
+#define DRM_RENDER_MINOR_BASE (DRM_NODE_RENDER * DRM_MAX_MINOR)
 
 static const struct backend {
    uint32_t context_type;
@@ -88,9 +95,59 @@ static const struct backend {
 #endif
 };
 
+static int
+open_render_node_for_driver(const char *driver_name)
+{
+   for (int minor = DRM_RENDER_MINOR_BASE;
+        minor < DRM_RENDER_MINOR_BASE + DRM_MAX_MINOR; minor++) {
+      char path[DRM_NODE_NAME_MAX];
+      int path_len = snprintf(path, sizeof(path), DRM_RENDER_DEV_NAME,
+                              DRM_DIR_NAME, minor);
+      if (path_len < 0 || (size_t)path_len >= sizeof(path))
+         return -ENAMETOOLONG;
+
+      int candidate = open(path, O_RDWR | O_CLOEXEC);
+      if (candidate < 0)
+         continue;
+
+      drmVersionPtr ver = drmGetVersion(candidate);
+      bool matches = ver && !strcmp(ver->name, driver_name);
+      if (ver)
+         drmFreeVersion(ver);
+      if (matches)
+         return candidate;
+
+      close(candidate);
+   }
+
+   return -ENODEV;
+}
+
+static int
+monotonic_time_ns(uint64_t *time_ns)
+{
+   struct timespec ts;
+
+   if (clock_gettime(CLOCK_MONOTONIC, &ts))
+      return -errno;
+
+   *time_ns = (uint64_t)ts.tv_sec * NSEC_PER_SEC + ts.tv_nsec;
+   return 0;
+}
+
 int
 drm_renderer_init(int drm_fd)
 {
+   uint64_t deadline_ns = 0;
+
+   if (drm_fd == -1) {
+      int ret = monotonic_time_ns(&deadline_ns);
+      if (ret)
+         return ret;
+      deadline_ns += DRM_RENDER_NODE_RESCAN_TIMEOUT_NS;
+   }
+
+retry:
    for (unsigned i = 0; i < ARRAY_SIZE(backends); i++) {
       const struct backend *b = &backends[i];
       int fd;
@@ -98,9 +155,11 @@ drm_renderer_init(int drm_fd)
       if (drm_fd != -1) {
          fd = drm_fd;
       } else {
-         fd = drmOpenWithType(b->name, NULL, DRM_NODE_RENDER);
-         if (fd < 0)
+         fd = open_render_node_for_driver(b->name);
+         if (fd == -ENODEV)
             continue;
+         if (fd < 0)
+            return fd;
       }
 
       drmVersionPtr ver = drmGetVersion(fd);
@@ -110,10 +169,9 @@ drm_renderer_init(int drm_fd)
       }
 
       if (strcmp(ver->name, b->name)) {
-         /* In the drmOpenWithType() path, we will only get back an fd
-          * for the device with matching name.  But when we are using
-          * an externally provided fd, we need to go thru the backends
-          * table to see which one has the matching name.
+         /* Direct render-node discovery only returns an fd for the requested
+          * driver.  An externally provided fd still needs to be matched
+          * against the backends table.
           */
          assert(drm_fd != -1);
          drmFreeVersion(ver);
@@ -134,8 +192,25 @@ drm_renderer_init(int drm_fd)
       return ret;
    }
 
-   if (drm_fd != -1)
+   if (drm_fd != -1) {
       close(drm_fd);
+   } else {
+      uint64_t now_ns = 0;
+      int ret = monotonic_time_ns(&now_ns);
+      if (ret)
+         return ret;
+
+      if (now_ns < deadline_ns) {
+         uint64_t delay_ns = MIN2(DRM_RENDER_NODE_RESCAN_INTERVAL_NS,
+                                  deadline_ns - now_ns);
+         struct timespec delay = {
+            .tv_sec = delay_ns / NSEC_PER_SEC,
+            .tv_nsec = delay_ns % NSEC_PER_SEC,
+         };
+         nanosleep(&delay, NULL);
+         goto retry;
+      }
+   }
 
    return -ENODEV;
 }