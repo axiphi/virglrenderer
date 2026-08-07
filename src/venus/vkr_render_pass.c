cb3a33b6d72db2393e927b3d958c2d052e187a75
Yiwei Zhang <zzyiwei@chromium.org>
2026-08-06 23:36:30 -0700

    vkr: add render_pass_2 create cmd gen support

--- src/venus/vkr_render_pass.c
+++ src/venus/vkr_render_pass.c
@@ -18,26 +18,7 @@ static void
 vkr_dispatch_vkCreateRenderPass2(struct vn_dispatch_context *dispatch,
                                  struct vn_command_vkCreateRenderPass2 *args)
 {
-   struct vkr_context *ctx = dispatch->data;
-   struct vkr_device *dev = vkr_device_from_handle(args->device);
-   struct vn_device_proc_table *vk = &dev->proc_table;
-
-   struct vkr_render_pass *pass = vkr_context_alloc_object(
-      ctx, sizeof(*pass), VK_OBJECT_TYPE_RENDER_PASS, args->pRenderPass);
-   if (!pass) {
-      args->ret = VK_ERROR_OUT_OF_HOST_MEMORY;
-      return;
-   }
-
-   vn_replace_vkCreateRenderPass2_args_handle(args);
-   args->ret = vk->CreateRenderPass2(args->device, args->pCreateInfo, NULL,
-                                     &pass->base.handle.render_pass);
-   if (args->ret != VK_SUCCESS) {
-      free(pass);
-      return;
-   }
-
-   vkr_device_add_object(ctx, dev, &pass->base);
+   vkr_render_pass_2_create_and_add(dispatch->data, args);
 }
 
 static void
